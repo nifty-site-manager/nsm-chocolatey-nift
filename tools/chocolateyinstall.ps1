3e3b663d0f4e721f86d555d3cf1ae50f8ba045b8
n-ham <nicholas.charles.ham@gmail.com>
2019-08-11 20:11:13 +1000

    used 32 bit compiler hopefully and changed install file

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,20 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
 $toolsDir   = "tools"
-$url        = 'https://github.com/nifty-site-manager/nsm-chocolatey/raw/master/nsm.exe' # download url, HTTPS preferred
 
 $packageArgs = @{
-  packageName   = 'nsm'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' #only one of these: exe, msi, msu
-  url           = $url
+  file          = "$toolsDir\nsm.exe"
 
   softwareName  = 'nsm'
-
-  # You can also use checksum.exe (choco install checksum) and use it 
-  # e.g. checksum -t sha256 -f path\to\file
-  checksum      = '01580AB337AEFEE80EAA970B479A0DD336C99CA88E62BF41B75794B446C74D61'
-  checksumType  = 'sha256' 
 
   # MSI
   silentArgs    = ""
